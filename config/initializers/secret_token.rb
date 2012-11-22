89cfde756ffd2fa528d947cad376d9481ba6d161
Kiattisak Anoochitarom <macbaszii@gmail.com>
2012-11-23 02:30:44 +0700

    themed by bootstrap and inseert navbar to form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InformationRetrieval::Application.config.secret_token = 'f29c38cc4d00b610704195975a65fc11b601a6bc2b5359560ac0bc4ff7c2f7ad225388ebf2c91d45a843c03e991b8b117708e638754e6ea75020a2a29dd2528b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+InformationRetrieval::Application.config.session_store :cookie_store, key: '_InformationRetrieval_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# InformationRetrieval::Application.config.session_store :active_record_store
